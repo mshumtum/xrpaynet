a8c0ecbf7315fd2094a1377a6505437f4a0ee263
Shubham06 <shubham.narula@antiersolutions.com>
2023-09-13 10:04:26 +0530

    working on create account screen

--- lib/components/screens/login/LoginScreen.dart
+++ lib/components/screens/login/LoginScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../../theme/Colors.dart';
 import '../../core/Locator.dart';
 import '../../core/navigation/navigation_service.dart';
+import '../../utilities/HexColor.dart';
 
 class LoginPage extends StatefulWidget {
   static const String routeName = '/login_page';
@@ -13,19 +15,104 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final NavigationService _navigationService = locator<NavigationService>();
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      backgroundColor: AppClr.black,
       appBar: AppBar(
+        toolbarHeight: 70,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back_ios_new_outlined,
+              color: Colors.white),
           onPressed: () => _navigationService.goBack(),
         ),
         title: const Text(""),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _titleText(),
+          _headingText(),
+          const SizedBox(height:30,),
+          _headingFieldText('Email'),
+          _emailInputField(),
+          _headingFieldText('Create Password'),
+          _emailInputField(),
+          _headingFieldText('Confirm Password'),
+          _emailInputField(),
+        ],
+      ),
+    );
+  }
+
+  Widget _titleText() {
+    return Container(
+      margin: const EdgeInsets.only(top: 40.0),
+      child: const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Create Account',
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontFamily: 'Roboto-Medium'),
+          )),
+    );
+  }
+
+  Widget _headingText() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10.0),
+      child:  Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Enter the required details below to \ncreate your account!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: HexColor("#B6B6B8"),
+                fontSize: 14,
+                fontFamily: 'Roboto-Regular',
+            ),
+          )),
+    );
+  }
+  Widget _headingFieldText(String text) {
+    return Container(
+      margin: const EdgeInsets.only(top: 5.0,left: 20.0),
+      child:  Text(
+        text,
+        textAlign: TextAlign.start,
+        style: TextStyle(
+          color: HexColor("#B6B6B8"),
+          fontSize: 14,
+          fontFamily: 'Roboto-Regular',
+        ),
+      ),
+    );
+  }
+  Widget _emailInputField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Username',
+          filled: true,
+          fillColor: const Color.fromRGBO(255, 0, 0, 1),
+          contentPadding:
+          const EdgeInsets.only(left: 14.0, bottom: 6.0, top: 8.0),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        keyboardType: TextInputType.emailAddress,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:xr_paynet/components/screens/WelcomeScreen.dart';
 import 'package:xr_paynet/theme/AppTheme.dart';
 import 'components/core/Locator.dart';
-import 'components/core/bloc_providers.dart';
 import 'components/core/navigation/navigation_service.dart';
 import 'components/core/navigation/routers.dart';
-import 'components/core/injection_container.dart' as di;
 
 
 
 Future<void> main() async {
-  // Service Locator Initialization
-  // await di.init();
   setupLocator();
   runApp(const MyApp());
 }
@@ -23,24 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // SystemChrome.setPreferredOrientations([
-    //   DeviceOrientation.portraitUp,
-    //   DeviceOrientation.portraitDown,
-    // ]);
-    // return MultiBlocProvider(
-    //   providers: BlocProviders.toGenerateProviders(),
-    //   child: Builder(
-    //     builder: (context) {
-    //       return MaterialApp(
-    //         home: const WelcomeScreen(),
-    //         navigatorKey: di.sl<NavigationService>().navigatorKey,
-    //         onGenerateRoute: (settings) =>
-    //             Routers.toGenerateRoute(settings),
-    //         debugShowCheckedModeBanner: false,
-    //       );
-    //     },
-    //   ),
-    // );
     return MaterialApp(
       title: 'Flutter UI',
       debugShowCheckedModeBanner: false,
