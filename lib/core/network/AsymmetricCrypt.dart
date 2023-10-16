7f9b066c933afe7b92b198c19075a0e39b566ae0
Shubham06 <shubham.narula@antiersolutions.com>
2023-10-16 15:38:34 +0530

    wokring on ancryption decryption

---
+++ lib/core/network/AsymmetricCrypt.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:encrypt/encrypt.dart';
+import 'package:encrypt/encrypt_io.dart';
+import 'package:pointycastle/asymmetric/api.dart';
+
+Future<void> encryptData(data) async {
+  final publicKey = await parseKeyFromFile<RSAPublicKey>('assets/pem/public_key.pem');
+  final privKey = await parseKeyFromFile<RSAPrivateKey>('assets/pem/private_key.pem');
+  final encrypter = Encrypter(RSA(publicKey: publicKey, privateKey: privKey));
+  final encrypted = encrypter.encrypt(data);
+  final decrypted = encrypter.decrypt(encrypted);
+  print(decrypted); // Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  print(encrypted.base64); // kO9EbgbrSwiq0EYz0aBdljHSC/rci2854Qa+nugbhKjidlezNplsEqOxR+pr1RtICZGAtv0YGevJBaRaHS17eHuj7GXo1CM3PR6pjGxrorcwR5Q7/bVEePESsimMbhHWF+AkDIX4v0CwKx9lgaTBgC8/yJKiLmQkyDCj64J3JSE=
+}
